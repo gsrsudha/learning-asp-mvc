0cc0f6ad77a045cbb2c70bbf8a316124c085c99e
gsrsudha <gsr.sudha8@gmail.com>
2018-12-03 16:04:55 -0600

    module 5 - working with forms

---
+++ CypherMVC/Models/ReplyVM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CypherMVC.Models
+{
+    public class ReplyVM
+    {
+        public virtual List<Message> Messages { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+        public int MessageThreadId { get; set; }
+    }
+}
---
+++ CypherMVC/Models/Vote.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace CypherMVC.Models
+{
+    public class Vote
+    {
+        [Key, ForeignKey("Admin")]
+        public int VoteId { get; set; }
+        public int VoteCount { get; set; }
+
+        public Admin Admin { get; set; }
+    }
+}
